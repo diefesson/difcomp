50d9c992e801eb61e0ccec4d776540b208ea8f72
diefesson <diefesson.so@gmail.com>
2022-06-28 19:57:14 -0300

    feat: modified rule to support prefix data

--- src/main/java/com/diefesson/difcomp/grammar/Rule.java
+++ src/main/java/com/diefesson/difcomp/grammar/Rule.java
@@ -1,34 +1,48 @@
 package com.diefesson.difcomp.grammar;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Rule {
 
     public final Element left;
+    private final List<Element> prefix;
     private final List<Element> right;
 
-    public Rule(Element left, List<Element> right) {
+    public Rule(Element left, List<Element> prefix, List<Element> right) {
         this.left = left;
+        this.prefix = prefix;
         this.right = right;
+    }
+
+    public Rule(Element left, List<Element> right) {
+        this(left, List.of(), right);
+    }
+
+    public List<Element> prefix() {
+        return Collections.unmodifiableList(prefix);
     }
 
     public List<Element> right() {
         return Collections.unmodifiableList(right);
     }
 
-    public boolean isEmpty() {
-        return right.get(0).type == ElementType.EMPTY;
+    public Rule advance() {
+        List<Element> newPrefix = new ArrayList<>(prefix);
+        List<Element> newRight = new ArrayList<>(right);
+        newPrefix.add(newRight.remove(0));
+        return new Rule(left, newPrefix, newRight);
     }
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(left.variable).append(" ->");
-        for (Element r : right) {
-            sb.append(" ").append(r);
+        StringBuilder builder = new StringBuilder();
+        builder.append(left.variable).append(" ->");
+        for (Element element : right) {
+            builder.append(" ").append(element);
         }
-        return sb.toString();
+        return builder.toString();
     }
 
     @Override
